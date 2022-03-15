6dbcfbb28e68b56b26f2921c3e086a1344ab8433
ranwenbo <ranwenbo@szzhilai.cn>
2022-03-15 10:26:54 +0800

    src\components\Test.vue

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,12 +1,5 @@
 <script setup>
-import {
-  ref,
-  reactive,
-  watch,
-  computed,
-  onMounted,
-  getCurrentInstance
-} from "vue";
+import { onUnmounted, getCurrentInstance, onUpdated } from "vue";
 const { proxy } = getCurrentInstance();
 const theme = proxy.$theme;
 function getRandomColor() {
@@ -19,12 +12,25 @@ function changeColor() {
     baseColor: getRandomColor()
   });
 }
-// setInterval(() => {
-//   changeColor();
-// }, 1000);
+const colorTimer = setInterval(() => {
+  changeColor();
+}, 1000);
+const clearColorTimer = () => {
+  if (colorTimer) {
+    clearInterval(colorTimer);
+  } else {
+    return;
+  }
+};
+onUpdated(() => {
+  clearColorTimer();
+});
+onUnmounted(() => {
+  clearColorTimer();
+});
 </script>
 <template>
-  <div class="test">10</div>
+  <div class="test"></div>
 </template>
 <style lang="scss" scoped>
 .test {
